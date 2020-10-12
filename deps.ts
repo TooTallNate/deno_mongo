be1776d42cc07287efface3e606ba5cb4cf6a5d9
Nathan Rajlich <n@n8.io>
2020-10-12 11:59:13 -0700

    Use `prepare()` fork

--- deps.ts
+++ deps.ts
@@ -1,1 +1,1 @@
-export { prepare } from "https://deno.land/x/plugin_prepare@v0.7.5/mod.ts";
+export { prepare } from "https://raw.githubusercontent.com/manyuanrong/deno-plugin-prepare/633ae0195058a9e3d9b5754fa0b785720382685e/mod.ts";
